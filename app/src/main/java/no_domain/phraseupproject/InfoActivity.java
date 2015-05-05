a8001ef8de7141aa7ec53001dc956682df5c656f
oocpawel <pawel@orderofcode.com>
2015-05-05 08:22:29 +0100

    Nowe widoki i konrolery: MenuActivity, InfoActivity, ChooseActivity

---
+++ app/src/main/java/no_domain/phraseupproject/InfoActivity.java
@@ -0,0 +1,28 @@
+package no_domain.phraseupproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class InfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+
+        //podpiecie sie pod przycisk "Got it"
+        Button backButton = (Button) findViewById(R.id.activity_info_button_back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //przekierowanie do MenuActivity
+                startActivityForResult(new Intent(InfoActivity.this, MenuActivity.class), 0);
+            }
+        });
+    }
+}
