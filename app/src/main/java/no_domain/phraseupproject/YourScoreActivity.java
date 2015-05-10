f4c671331b6147a1d3712d6c0bfdb35b4a5e3fde
oocpawel <pawel@orderofcode.com>
2015-05-10 11:01:17 +0100

    small refactoring

--- app/src/main/java/no_domain/phraseupproject/YourScoreActivity.java
+++ app/src/main/java/no_domain/phraseupproject/YourScoreActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 
 public class YourScoreActivity extends Activity {
@@ -15,26 +14,16 @@ public class YourScoreActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_score);
 
+
+
         //podpiecie sie pod przycisk "Back"
         Button backButton = (Button) findViewById(R.id.activity_choose_button_back);
         backButton.setOnClickListener(new View.OnClickListener() {
-
-
-
-
             @Override
             public void onClick(View v) {
                 //przekierowanie do MenuActivity
                 startActivityForResult(new Intent(YourScoreActivity.this, MenuActivity.class), 0);
-
-
             }
-
-
         });
-
-
     }
-
-
-    }
+}
